0e892a88fe0b44a0ce23a5e69099e85fcf5bce4c
jrobertofg <jrobertofg@gmail.com>
2024-03-07 17:01:48 -0600

    Add files via upload

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -3,8 +3,9 @@ import csv
 
 Poll_path = os.path.join('Resources', 'election_data.csv')
 output_file_path = os.path.join('Resources', 'election_results.txt')
+
 # Count number of votes
-unique_votes = set()
+total_votes = 0
 candidates_votes = {}
 
 with open(Poll_path, 'r') as csvfile:
@@ -12,20 +13,21 @@ with open(Poll_path, 'r') as csvfile:
     rows = list(csvreader)
     header = rows[0]
 
-    # Assuming the column index for 'Candidate' is 2, adjust if needed
+    # Assuming the column index for 'Candidate' is 2 and 'Votes' is 0, adjust if needed
+    vote_column_index = 0
     candidate_column_index = 2
 
     # Iterate through rows and extract vote information
     for row in rows[1:]:
-        vote = row[candidate_column_index]
-        unique_votes.add(vote)
+        vote = row[vote_column_index]
+        total_votes += 1
 
-        if vote in candidates_votes:
-            candidates_votes[vote] += 1
+        candidate = row[candidate_column_index]
+
+        if candidate in candidates_votes:
+            candidates_votes[candidate] += 1
         else:
-            candidates_votes[vote] = 1
-
-total_votes = len(unique_votes)
+            candidates_votes[candidate] = 1
 
 print("Election Results")
 print("-------------------")
@@ -53,4 +55,3 @@ with open(output_file_path, 'w') as output_file:
         output_file.write(f'{candidate}: {votes} votes ({percentage:.2f}%)\n')
     output_file.write("-------------------\n")
     output_file.write(f'Winner: {winner}\n')
-    
